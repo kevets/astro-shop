a3b295f63b5f222473b1556f46ae76d069de0b78
Steve King <kevets@gmail.com>
2025-12-20 11:20:41 -0600

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "SBASP SHOP";
-export const SITE_DESCRIPTION = "Steve In Texas";
+export const SITE_TITLE = "SBS STORE";
+export const SITE_DESCRIPTION = "Small Batch Steve - 2025";
