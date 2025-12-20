589a876ffa7692b02d241b2c6baadb805b5f1bfa
Steve King <kevets@gmail.com>
2025-12-20 11:23:03 -0600

    Update consts.ts
    update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "SBS STORE";
-export const SITE_DESCRIPTION = "Small Batch Steve - 2025";
+export const SITE_TITLE = "Small Batch Steve's Seasonings";
+export const SITE_DESCRIPTION = "Welcome to our shop page!";
